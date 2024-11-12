349beaee82e23760fa1b0bf7a7b0b17f5f8003a0
Wesley Watkins <a03291354@usu.edu>
2024-11-12 12:49:08 -0700

    Final merge change fix

--- codepop/src/pages/PostCheckout.js
+++ codepop/src/pages/PostCheckout.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, Button } from 'react-native';
+import { View, Text, StyleSheet, Image, Button, TouchableOpacity } from 'react-native';
 import NavBar from '../components/NavBar';
 
 // todo
