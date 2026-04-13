31b9d9390b1b3ebea5bc1d272d5be773eb20a6e3
A02368805 <a02368805@usu.edu>
2026-04-13 09:19:15 -0600

    Revert "Update inventory adjust test to match new empty-default form behavior"
    This reverts commit 62ef6c7d3b83b808acb17d3b21c809807927d14c.

--- server/tests/test_views.py
+++ server/tests/test_views.py
@@ -870,7 +870,7 @@ class DashboardAndHtmxViewTests(TestCase):
         self.assertEqual(syrup_response.status_code, 200)
         self.assertContains(
             syrup_response,
-            'name="delta" step="0.01" placeholder="Delta"',
+            'name="delta" step="0.01" value="1"',
             html=False,
         )
 
@@ -882,7 +882,7 @@ class DashboardAndHtmxViewTests(TestCase):
         self.assertEqual(cup_response.status_code, 200)
         self.assertContains(
             cup_response,
-            'name="delta" step="1" placeholder="Delta"',
+            'name="delta" step="1" value="1"',
             html=False,
         )
 
